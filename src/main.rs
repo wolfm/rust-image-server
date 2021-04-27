fb354c3674c706214f4503be375da8cdb6ef2b6b
Michael Wolf <mjwolf@umich.edu>
2021-04-27 12:56:30 -0400

    Send any request UTF-8 file

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,15 @@ use std::net::TcpStream;
 
 fn main() {
     // Get arguments
-    let args: Vec<String> = env::args().collect();
-
-    let root_path = &args[1];
-    println!("Base path: {}", root_path); 
+    // let args: Vec<String> = env::args().collect();
+    // let root_path = &args[1];
+    // println!("Base path: {}", root_path); 
 
     // Setup TCP Listener
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection received");
         handle_connection(stream);
     }
 }
@@ -35,26 +33,37 @@ fn handle_connection(mut stream: TcpStream) {
     let mut iter = buffer_in.split_whitespace();
     let req_method = iter.next();
     let path = iter.next();
-    let http_version = iter.next();
+    // let http_version = iter.next();
 
+    // Get response file based on request
     let (status, filename) = match (req_method, path) {
         (None, _) | (_, None) => ("400 Bad Request", "400.html"),
-        (Some("GET"), Some("/index.html")) => ("200 OK", "index.html"), 
-        (Some("GET"), _) => ("404 NOT FOUND", "404.html"),
+        (Some("GET"), Some("/index.html")) | (Some("GET"), Some("/")) => ("200 OK", "index.html"), 
+        (Some("GET"), _) => 
+            match (path) {
+                Some(path_str) => {
+                    let path_slice = &path_str[1..];
+                    if std::path::Path::new(path_slice).exists() {
+                        ("200 OK", path_slice)
+                    }
+                    else { ("404 NOT FOUND", "404.html") }
+                },
+                None =>  ("404 NOT FOUND", "404.html")
+            },
         _ => ("501 Not Implemented", "501.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    println!("{:?}", (status, filename));
 
+    // Write response
+    // TODO fix so that can GET .jpeg files. Currently read_to_string cannot read non-UTF-8
+    let contents = fs::read_to_string(filename).unwrap();
     let response = format!(
         "HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
         status,
         contents.len(),
         contents
     );
-
-    // println!("{}", response);
-
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
